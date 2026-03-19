7a3f34b39efa6dcbfa47fc1873c6b1405b8b6022
Bmaxx12 <ardanputra405@gmail.com>
2026-03-20 02:23:50 +0700

    feat: rebuild ai

--- gemini_helper.py
+++ gemini_helper.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
+
 SYSTEM_INSTRUCTION = """
 Kamu adalah asisten keuangan khusus untuk UMKM Indonesia bernama PANTAUIN.
 Kamu HANYA boleh menjawab pertanyaan yang berkaitan dengan:
